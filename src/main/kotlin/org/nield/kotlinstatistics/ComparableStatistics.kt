06b932ab712f7598de72825f19e84a09ed606f97
Thomas Nield <thomasnield@live.com>
2017-03-09 00:00:50 -0600

    add countBy()

--- src/main/kotlin/org/nield/kotlinstatistics/ComparableStatistics.kt
+++ src/main/kotlin/org/nield/kotlinstatistics/ComparableStatistics.kt
@@ -1,8 +1,6 @@
 package org.nield.kotlinstatistics
 
-fun <T> Sequence<T>.mode() = groupBy { it }
-        .entries.asSequence()
-        .map { it.key to it.value.count() }
+fun <T> Sequence<T>.mode() = countBy()
         .sortedByDescending { it.second }
         .toList().let { list ->
             list.asSequence()
@@ -18,3 +16,21 @@ fun IntArray.mode() = asIterable().mode()
 fun LongArray.mode() = asIterable().mode()
 fun FloatArray.mode() = asIterable().mode()
 fun DoubleArray.mode() = asIterable().mode()
+
+/**
+ * Emits each distinct value with the number counts it appeared
+ */
+fun <T> Sequence<T>.countBy() =
+        groupBy { it }
+        .entries.asSequence()
+        .map { it.key to it.value.count() }
+
+
+fun <T> Iterable<T>.countBy() = asSequence().countBy()
+
+fun <T,R> Sequence<T>.countBy(selector: (T) -> R) =
+        groupBy { selector(it) }
+                .entries.asSequence()
+                .map { it.key to it.value.count() }
+
+fun <T,R> Iterable<T>.countBy(selector: (T) -> R) = asSequence().countBy(selector)
