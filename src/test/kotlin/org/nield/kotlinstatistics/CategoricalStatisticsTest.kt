0ab119db584e3835040ec7c9fecb399323932175
Thomas Nield <thomasnield@live.com>
2017-03-09 00:03:47 -0600

    rename to Categorical

--- src/test/kotlin/org/nield/kotlinstatistics/CategoricalStatisticsTest.kt
+++ src/test/kotlin/org/nield/kotlinstatistics/CategoricalStatisticsTest.kt
@@ -3,7 +3,7 @@ package org.nield.kotlinstatistics
 import junit.framework.Assert.assertTrue
 import org.junit.Test
 
-class ComparableStatisticsTest {
+class CategoricalStatisticsTest {
     @Test
     fun testMode1() {
         assertTrue(listOf(2,54,67,3,4,5,2,2).mode().toSet() == setOf(2))
