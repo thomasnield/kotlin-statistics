64478708479bb3de5f67dd85928e5d3a47e1b182
Thomas Nield <thomasnield@live.com>
2017-07-01 23:38:58 -0500

    implement other clustering methodologies

--- src/main/kotlin/org/nield/kotlinstatistics/Clustering.kt
+++ src/main/kotlin/org/nield/kotlinstatistics/Clustering.kt
@@ -1,12 +1,14 @@
 package org.nield.kotlinstatistics
 
 import org.apache.commons.math3.ml.clustering.Clusterable
+import org.apache.commons.math3.ml.clustering.DBSCANClusterer
+import org.apache.commons.math3.ml.clustering.FuzzyKMeansClusterer
 import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer
 
-fun Collection<Pair<Double,Double>>.cluster(k: Int, maxIterations: Int) = cluster(k, maxIterations, {it.first}, {it.second})
-fun Sequence<Pair<Double,Double>>.cluster(k: Int, maxIterations: Int) = toList().cluster(k, maxIterations, {it.first}, {it.second})
+fun Collection<Pair<Double,Double>>.kMeansCluster(k: Int, maxIterations: Int) = kMeansCluster(k, maxIterations, {it.first}, {it.second})
+fun Sequence<Pair<Double,Double>>.kMeansCluster(k: Int, maxIterations: Int) = toList().kMeansCluster(k, maxIterations, {it.first}, {it.second})
 
-inline fun <T> Collection<T>.cluster(k: Int, maxIterations: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+inline fun <T> Collection<T>.kMeansCluster(k: Int, maxIterations: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
         asSequence().map { ClusterInput(it, doubleArrayOf(xSelector(it), ySelector(it))) }
                 .toList()
                 .let {
@@ -16,8 +18,37 @@ inline fun <T> Collection<T>.cluster(k: Int, maxIterations: Int, crossinline xSe
                             Centroid((it.center).point.let { DoublePoint(it[0],it[1])}, it.points.map { it.item })
                         }
                 }
-inline fun <T> Sequence<T>.cluster(k: Int, maxIterations: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
-        toList().cluster(k,maxIterations,xSelector,ySelector)
+inline fun <T> Sequence<T>.kMeansCluster(k: Int, maxIterations: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+        toList().kMeansCluster(k,maxIterations,xSelector,ySelector)
+
+
+
+inline fun <T> Collection<T>.fuzzyKMeansCluser(k: Int, fuzziness: Double, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+        asSequence().map { ClusterInput(it, doubleArrayOf(xSelector(it), ySelector(it))) }
+                .toList()
+                .let {
+                    FuzzyKMeansClusterer<ClusterInput<T>>(k,fuzziness)
+                            .cluster(it)
+                            .map {
+                                Centroid((it.center).point.let { DoublePoint(it[0],it[1])}, it.points.map { it.item })
+                            }
+                }
+inline fun <T> Sequence<T>.fuzzyKMeansCluser(k: Int, fuzziness: Double, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+        toList().fuzzyKMeansCluser(k,fuzziness,xSelector,ySelector)
+
+
+inline fun <T> Collection<T>.dbScanCluster(maximumRadius: Double, minPoints: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+        asSequence().map { ClusterInput(it, doubleArrayOf(xSelector(it), ySelector(it))) }
+                .toList()
+                .let {
+                    DBSCANClusterer<ClusterInput<T>>(maximumRadius,minPoints)
+                            .cluster(it)
+                            .map {
+                                Centroid(DoublePoint(-1.0,-1.0), it.points.map { it.item })
+                            }
+                }
+inline fun <T> Sequence<T>.dbScanCluster(maximumRadius: Double, minPoints: Int, crossinline xSelector: (T) -> Double, crossinline ySelector: (T) -> Double) =
+        toList().dbScanCluster(maximumRadius,minPoints,xSelector,ySelector)
 
 class ClusterInput<out T>(val item: T, val location: DoubleArray): Clusterable {
     override fun getPoint() = location
