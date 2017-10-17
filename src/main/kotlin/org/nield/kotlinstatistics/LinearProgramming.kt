9f5825d32454c837de87642eb2d1722b6dc72cdd
Thomas Nield <thomasnield@live.com>
2017-10-16 21:47:02 -0500

    start implementing linear optimization

--- src/main/kotlin/org/nield/kotlinstatistics/LinearProgramming.kt
+++ src/main/kotlin/org/nield/kotlinstatistics/LinearProgramming.kt
@@ -12,12 +12,14 @@ class Optimizer(vector: RealVector, constant: Double = 0.0) {
     private val f = LinearObjectiveFunction(vector, constant)
     private val constraints = mutableListOf<LinearConstraint>()
 
-
     fun constraint(coefficients: DoubleArray, relationship: Relationship, value: Double) {
         constraints += LinearConstraint(coefficients, relationship, value)
     }
     fun constraint(vector: RealVector, relationship: Relationship, value: Double) {
         constraints += LinearConstraint(vector, relationship, value)
+    }
+    fun constraint(linearConstraint: LinearConstraint) {
+        constraints += linearConstraint
     }
     fun maximize(negativeAllowed: Boolean = false, maxIterations: Int = Integer.MAX_VALUE) =
             SimplexSolver().optimize(MaxIter(maxIterations), f, LinearConstraintSet(constraints), GoalType.MAXIMIZE, NonNegativeConstraint(!negativeAllowed))
@@ -34,3 +36,7 @@ fun optimizer(coefficients: RealVector, constant: Double = 0.0, op: Optimizer.()
         Optimizer(coefficients, constant).apply(op)
 
 fun v(vararg vectorValues: Double) = ArrayRealVector(vectorValues)
+
+infix fun RealVector.GEQ(value: Double) = LinearConstraint(this,Relationship.GEQ,value)
+infix fun RealVector.LEQ(value: Double) = LinearConstraint(this,Relationship.LEQ,value)
+infix fun RealVector.EQ(value: Double) = LinearConstraint(this,Relationship.EQ,value)
